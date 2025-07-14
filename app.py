d5584f7daf7337fc3291f3d491a5f5263ded3618
Lucas <100660343+lulunac27a@users.noreply.github.com>
2025-07-14 22:46:35 +0200

    Add more comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#simple Python app
 print('This is a simple Python app.')#print simple text
 input_text = input('Enter text: ')#prompt text for input
 print(input_text)#print variable to entered text input

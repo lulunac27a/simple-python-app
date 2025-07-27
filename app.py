7aa0b080019af12de5e72fd6c55e4870dd656f69
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-07-26 20:47:51 -0500

    Format code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-#a simple Python app
-print('This is a simple Python app.')#print simple text
-input_text = input('Enter text: ')#prompt text for input
-print(input_text)#print variable to entered text input
-print('Length: ', len(input_text))#print length of entered text
-print('Uppercase output: ', input_text.upper())#print text in uppercase
-print('Lowercase output: ', input_text.lower())#print text in lowercase
-print('Titlecase output: ', input_text.title())#print text in titlecase
-print('Capitalized output: ', input_text.capitalize())#print text in capitalized (first letter only)
-print('Reversed output: ', input_text[::-1])#print text in reverse
+# a simple Python app
+print("This is a simple Python app.")  # print simple text
+input_text = input("Enter text: ")  # prompt text for input
+print(input_text)  # print variable to entered text input
+print("Length: ", len(input_text))  # print length of entered text
+print("Uppercase output: ", input_text.upper())  # print text in uppercase
+print("Lowercase output: ", input_text.lower())  # print text in lowercase
+print("Titlecase output: ", input_text.title())  # print text in titlecase
+print(
+    "Capitalized output: ", input_text.capitalize()
+)  # print text in capitalized (first letter only)
+print("Reversed output: ", input_text[::-1])  # print text in reverse
